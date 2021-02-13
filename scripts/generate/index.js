5dffb626027854cab44488f454401ff133f8afaf
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-02-13 20:58:48 -0300

    refactor(scripts/generate): split into modules

--- scripts/generate/index.js
+++ scripts/generate/index.js
@@ -1,79 +1,17 @@
 import { existsSync, mkdirSync } from 'fs'
 import { join } from 'path'
-import { packagesPath } from '../../utils/packages-path'
 
 import chalk from 'chalk'
-import execa from 'execa'
-import writePackage from 'write-pkg'
+
+import { createDirectory } from './create-directory'
+import { createPackageJson } from './create-package-json'
+import { initialCommit } from './initial-commit'
+import { tag } from './tag'
+import { push } from './push'
+import { packagesPath } from '../../utils/packages-path'
 
 const { log } = console
 const dryRun = process.argv.includes('--dry-run')
-const noPush = process.argv.includes('--no-push')
-
-const createDirectory = cwd => {
-  if (dryRun) {
-    log(chalk`{yellow Skipping directory creation}`)
-    return
-  }
-
-  log(chalk`{blue Creating directory}`)
-  mkdirSync(cwd)
-}
-
-const createPackageJson = (cwd, packageName) => {
-  if (dryRun) {
-    log(chalk`{yellow Skipping package.json creation}`)
-    return
-  }
-
-  const packageJsonPath = join(cwd, 'package.json')
-  const packageJsonContent = {
-    name: `area51-semver-changelog-monorepo-${packageName}`,
-    version: '0.0.0'
-  }
-  writePackage(packageJsonPath, packageJsonContent)
-}
-
-const initialCommit = async (cwd, packageName) => {
-  if (dryRun) {
-    log(chalk`{yellow Skipping initial commit}`)
-    return
-  }
-
-  let params = ['add', cwd]
-  await execa('git', params)
-
-  log(chalk`{blue Committing package.json}`)
-
-  params = [
-    'commit',
-    '-m',
-    `chore(release): area51-semver-changelog-monorepo-${packageName} v0.0.0`
-  ]
-  await execa('git', params)
-}
-
-const tag = async (cwd, packageName) => {
-  if (dryRun) {
-    log(chalk`{yellow Skipping Git tag}`)
-    return
-  }
-
-  const tagName = `area51-semver-changelog-monorepo/${packageName}-v0.0.0`
-  log(chalk`\n{blue Tagging} {grey ${tagName}}`)
-  await execa('git', ['tag', tagName], { cwd, stdio: 'inherit' })
-}
-
-export const push = async () => {
-  if (dryRun || noPush) {
-    log(chalk`{yellow Skipping Git push}`)
-    return
-  }
-
-  log(chalk`{blue Pushing release and tags}`)
-  await execa('git', ['push'])
-  await execa('git', ['push', '--tags'])
-}
 
 try {
   const [, , packageName] = process.argv
