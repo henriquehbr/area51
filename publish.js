d0a361207a964701db5d67997f16ab335345ed9c
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-02-26 19:52:23 -0300

    chore: use default import instead named on `pacote`

--- publish.js
+++ publish.js
@@ -1,10 +1,10 @@
 import { publish } from 'libnpmpublish'
-import { manifest, tarball } from 'pacote'
+import pacote from 'pacote'
 
 import { name, version } from './package.json'
 
-const manifest = await manifest(process.cwd())
-const tarData = await tarball(process.cwd())
+const manifest = await pacote.manifest(process.cwd())
+const tarData = await pacote.tarball(process.cwd())
 
 await publish(manifest, tarData, {
   npmVersion: `${name}@${version}`
